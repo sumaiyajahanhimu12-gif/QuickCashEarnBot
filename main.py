c7dfc374501d48d9b2cfa3125be99cddb1d7f365
sumaiyajahanhimu12-gif <sumaiyajahanhimu12@gmail.com>
2026-07-24 22:01:35 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("Quick Cash Earn Bot")
+import os
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "🎉 Welcome to Quick Cash Earn\n\nBot is running successfully."
+    )
+
+def main():
+    app = Application.builder().token(BOT_TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+
+    print("Bot Started...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
